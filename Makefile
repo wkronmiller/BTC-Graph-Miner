1729248fa3454ed8dc2cdf2733051ad80fc13ca3
William Rory Kronmiller <wrk961@gmail.com>
2017-04-24 16:40:12 -0400

    Add MPI

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all: src/main.c
 	mpicc -g -I. -Wall -O3 src/main.c -o a.out
+test8:
+	mpirun -np 8 ./a.out
--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
+#include<stdlib.h>
 #include<stdio.h>
+#include<mpi.h>
 
 int main(int argc, char ** argv) {
-    printf("Hello world\n");
-    return 1; //TODO
+    // Initialize MPI
+    int mpi_commsize, mpi_myrank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_myrank);
+
+    printf("[%u/%u]: Hello world\n", mpi_myrank, mpi_commsize);
+
+    // Exit MPI
+    MPI_Finalize();
+    return EXIT_SUCCESS;
 }
