921f5b6703d23a4a9c97687f5fca7e63ae955ca1
root <root@nero.kronmiller.net>
2017-04-27 17:38:23 -0400

    Break thigns

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ all: src/main.c
 	mpicc -g -I. -Wall -O3 src/main.c -o a.out
 test8:
 	mpirun -np 8 ./a.out testInput/testInput
+test8big:
+	mpirun -np 16 ./a.out /mnt/BitcoinDrive/1ktransactions
--- src/main.c
+++ src/main.c
@@ -2,12 +2,44 @@
 #include<stdio.h>
 #include<assert.h>
 #include<mpi.h>
-#include<dirent.h>
+#include<string.h>
+
+int mpi_commsize, mpi_myrank;
 
 typedef struct TransactionsStrings {
     size_t size;
     char * buffer;
 } TransactionsStrings;
+
+#define HASH_BITS 160
+#define HASH_MAX_LENGTH HASH_BITS / 4
+#define LONGS_PER_HASH (HASH_BITS / 64) + 1
+#define INPUTS_PER_TRANSACTION_MAX 10
+#define OUTPUTS_PER_TRANSACTION_MAX 20
+
+// A single bitcoin address
+typedef struct Address {
+    unsigned long long int chunks[LONGS_PER_HASH];
+} Address;
+
+// Transaction hash storage
+typedef struct Transaction {
+    unsigned int num_inputs;
+    unsigned int num_outputs;
+    Address * inputs;
+    Address * outputs;
+} Transaction;
+
+// List of transactions
+typedef struct Transactions {
+    Transaction * transactions;
+    unsigned int num_transactions;
+} Transactions;
+
+// Default checked function error-handler
+void handleError(int err) {
+    //TODO
+}
 
 void seekToNewline(MPI_File * p_fh) {
     int err;
@@ -15,6 +47,7 @@ void seekToNewline(MPI_File * p_fh) {
     MPI_Status read_status;
     while(buf[0] != '\n') {
         err = MPI_File_read(*p_fh, &buf[0], 1, MPI_CHAR, &read_status);
+        handleError(err);
     }
 }
 
@@ -24,10 +57,12 @@ void loadRankData(const char * source_file, const int mpi_commsize, const int mp
     // Open file
     MPI_File fh;
     err = MPI_File_open(MPI_COMM_WORLD, source_file, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+    handleError(err);
 
     // Get file size
     MPI_Offset file_size;
     err = MPI_File_get_size(fh, &file_size);
+    handleError(err);
 
     // Get current rank's starting point
     const MPI_Offset rank_chunk_size = (file_size / mpi_commsize);
@@ -39,6 +74,7 @@ void loadRankData(const char * source_file, const int mpi_commsize, const int mp
     }
     // Set current rank's view
     err = MPI_File_set_view(fh, start_offset, MPI_CHAR, MPI_CHAR, "external32", MPI_INFO_NULL);
+    handleError(err);
     if(mpi_myrank != 0) {
         seekToNewline(&fh);
     }
@@ -46,6 +82,7 @@ void loadRankData(const char * source_file, const int mpi_commsize, const int mp
     // Get starting position
     MPI_Offset current_position;
     err = MPI_File_get_position(fh, &current_position);
+    handleError(err);
 
     // Load safe chunks
     const size_t inner_buffer_size = end_offset - start_offset - current_position;
@@ -53,9 +90,11 @@ void loadRankData(const char * source_file, const int mpi_commsize, const int mp
     inner_buffer[inner_buffer_size] = '\0';
     MPI_Status read_status;
     err = MPI_File_read(fh, inner_buffer, inner_buffer_size, MPI_CHAR, &read_status);
+    handleError(err);
 
     // Ensure all inner data is read
     err = MPI_File_get_position(fh, &current_position);
+    handleError(err);
     assert(current_position + start_offset == end_offset);
 
     // Load remainder until newline
@@ -63,6 +102,7 @@ void loadRankData(const char * source_file, const int mpi_commsize, const int mp
     char * outer_buffer = malloc(sizeof(char) * 2048 + 1);
     do {
         err = MPI_File_read(fh, &outer_buffer[outer_buffer_position], 1, MPI_CHAR, & read_status);
+        handleError(err);
     } while(outer_buffer[outer_buffer_position++] != '\n');
     outer_buffer[outer_buffer_position] = '\0';
 
@@ -82,9 +122,67 @@ void loadRankData(const char * source_file, const int mpi_commsize, const int mp
     MPI_File_close(&fh);
 }
 
+unsigned int splitString(char c, char * string, char ** tokenized) {
+    const unsigned int string_length = strlen(string);
+    unsigned int num_tokens = 0;
+    unsigned int str_index;
+    unsigned int segment_start = 0;
+    char * token;
+    for(str_index = 0; str_index < string_length; ++str_index) {
+        if(string[str_index] == c) {
+            string[str_index] = '\0';
+            token = &string[segment_start];
+            tokenized[num_tokens++] = token;
+            segment_start = str_index + 1;
+        }
+    }
+    if(segment_start < string_length) {
+        token = &string[segment_start];
+        tokenized[num_tokens++] = &string[segment_start];
+    }
+    return num_tokens;
+}
+
+void parseTransaction(char * transaction_line, Transaction * p_transaction) {
+    char * split_transaction[2];
+    if(mpi_myrank == 15) {
+        fprintf(stderr, "%s\n", transaction_line);
+    }
+    const int num_tokens = splitString(';', transaction_line, &split_transaction[0]);
+    if(num_tokens != 2) {
+        fprintf(stderr, "Failed to split '%s'['%s' '%s'](%u tokens)\n", transaction_line, split_transaction[0], split_transaction[1], num_tokens);
+        fprintf(stderr, "Failed rank: %u\n", mpi_myrank);
+        sleep(10);
+        abort();
+    }
+    //TODO
+}
+
+void parseRankData(TransactionsStrings * p_tstrs, Transactions * p_transactions) {
+    // Split string of transactions
+    char ** tokenized_lines = malloc(sizeof(char *) * p_tstrs->size);
+    p_transactions->num_transactions = splitString('\n', p_tstrs->buffer, tokenized_lines);
+    // Allocate space to store transaction data
+    p_transactions->transactions = malloc(sizeof(Transaction) * p_transactions->num_transactions);
+    // Iterate over individual transaction strings
+    unsigned int line_index;
+    for(line_index = 0; line_index < p_transactions->num_transactions; ++line_index) {
+        parseTransaction(tokenized_lines[line_index], &(p_transactions->transactions[line_index]));
+    }
+    free(tokenized_lines);
+}
+
+//TODO: create lookup function based on number of ranks and hash
+
+/**
+ * Can check for messages using MPI_Probe http://mpitutorial.com/tutorials/dynamic-receiving-with-mpi-probe-and-mpi-status/
+ * But need to specify sender
+ *
+ * Could also send message to node saying not to expect any more messages
+ */
+
 int main(int argc, char ** argv) {
     // Initialize MPI
-    int mpi_commsize, mpi_myrank;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_myrank);
@@ -95,12 +193,11 @@ int main(int argc, char ** argv) {
     TransactionsStrings tstrs;
     loadRankData(source_file, mpi_commsize, mpi_myrank, &tstrs);
 
-    if(mpi_myrank == 0) {
-        // Debug output
-        printf("%s\n", tstrs.buffer);
-    }
+    Transactions transactions;
+    parseRankData(&tstrs, &transactions);
 
     // Clean up
+    free(transactions.transactions);
     free(tstrs.buffer);
 
     // Exit MPI
